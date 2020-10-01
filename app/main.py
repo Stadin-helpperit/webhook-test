00e3a8737858bf1176436b49c29a4dab37be463b
Daniel Buselli <buselli.danielgmail.com>
2020-10-01 15:16:31 +0300

    testing out user interaction

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import os
 app = Flask(__name__)
 
@@ -6,6 +6,24 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "Hello World!"
+
+
+@app.route('/webhook', methods=['POST', 'GET'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    fulfillmentText = 'Success'
+
+    query_result = req.get('query_Result')
+
+    userInput = query_result.get('parameters').get('telegram-command')
+
+    print(userInput)
+
+    return {
+        "fulfillmentText": fulfillmentText
+
+    }
 
 
 if __name__ == '__main__':
